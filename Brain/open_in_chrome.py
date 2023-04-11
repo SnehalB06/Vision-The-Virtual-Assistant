25664f28bc15295532480161dbc2a4126a381a49
Shubham Kulkarni <shubhamkulk@gmail.com>
2023-04-11 17:06:19 -0400

    done and done

--- Brain/open_in_chrome.py
+++ Brain/open_in_chrome.py
@@ -8,34 +8,34 @@ applications_list = ['google', 'facebook', 'instagram']
 def replace_keyword(word, query):
     return query.replace(word, "")
 
-response_success = "The Command has been executed."
+response_success = "The command has been executed."
 response_failure = "That dind't work. Please try again"
+
+def run_command(Nameofweb):
+    Link = f"https://www.{Nameofweb}.com"
+    webbrowser.open(Link)
 
 def processOpenQuery(Query):
     Query = str(Query).lower()
 
     if "visit" in Query:
         Nameofweb = replace_keyword('visit', Query).strip()
-        Link = f"https://www.{Nameofweb}.com"
-        webbrowser.open(Link)
+        run_command(Nameofweb)
         return response_success
     
     elif "open" in Query:
         Nameofweb = replace_keyword('open', Query).strip()
-        Link = f"https://www.{Nameofweb}.com"
-        webbrowser.open(Link)
+        run_command(Nameofweb)
         return response_success
 
     elif "launch" in Query:
         Nameofweb = replace_keyword('launch', Query).strip()
-        Link = f"https://www.{Nameofweb}.com"
-        webbrowser.open(Link)
+        run_command(Nameofweb)
         return response_success
 
     elif "start" in Query:
-        Nameoftheapp= replace_keyword('launch', Query).strip()
-        if "chrome" in Nameoftheapp:
-            os.open(r"/Applications/Google Chrome")
+        Nameofweb= replace_keyword('launch', Query).strip()
+        run_command(Nameofweb)
         return response_success
     
     else:
